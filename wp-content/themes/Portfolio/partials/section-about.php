3d0b51a9ab348d98e957797ee0162d01e56fed11
Y.F <younfarhane@gmail.com>
2018-04-30 22:09:03 -0400

    POrtfolio : front end - section about

--- wp-content/themes/Portfolio/partials/section-about.php
+++ wp-content/themes/Portfolio/partials/section-about.php
@@ -6,10 +6,9 @@
                 <div class="row">
                     <div class="col-1-of-2">
                         <h3 class="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
-                        <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                            ducimus quam nisi exercitationem omnis earum qui.</p>
-                            <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur libero repellat quis consequatur
-                            ducimus quam nisi exercitationem omnis earum qui.</p>
+                        <p class="paragraph">Je suis Younes Farhane originaire du Maroc, développeur front-end basé à Montreal. Je suis passionné par le Web et par toutes nouvelles technologies. C’est ainsi que j'ai décidé d'assouvir ma passion en m'orientant dans un programme de technique d'intégration multimédia en Art et technologie des médias au Cégep de Jonquière au Québec.</p>
+                        <p class="paragraph"> Développeur web front-end depuis un an et demi, j'ai acquis de l'experience professionnelle en développant des sites web au design fluide, que se soit pour ordinateur, tablette ou téléphone intelligent, tout en respectant les dernières normes W3C. J'utilise les plateformes telles que HTML5/CSS3, le framework BOOTSTRAP ainsi que le cms WORDPRESS</p>
+                        <p class="paragraph"> Passionné de programmation web, je me tiens quotidiennement à l'affût des nouvelles technologies pour permettre de développer des sites web selon les besoins des clients.</p>
                     </div>
                     <div class="col-1-of-2">
                         <div class="composition">
